d060deacb0ffc6bcbc1ba7bbe30632ef1ead2f7f
Elena <elena@elenas-air.gateway.pace.com>
2017-09-18 19:19:30 -0400

    3

--- server.js
+++ server.js
@@ -26,9 +26,15 @@ app.get('/about', function(req, res) {
 });
 
 
-var port = process.env.port || 8080;
+//var port = process.env.port || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-app.listen(port, function () {
+//app.listen(port, function () {
   //console.log('Express http server listening on ' + port)
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-})
+  //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//})
+
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
