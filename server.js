a78443471819b769fa1b3c831432ac53d367e3d3
Elena <elena@elenas-air.gateway.pace.com>
2017-09-23 14:04:04 -0400

    1

--- server.js
+++ server.js
@@ -9,6 +9,8 @@
 *		Online	(Heroku)	Link:	https://lit-retreat-28570.herokuapp.com/
 *
 ********************************************************************************/	
+var dataService = require('/data-service.js');
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -25,14 +27,22 @@ app.get('/about', function(req, res) {
 res.sendFile(path.join(__dirname + '/views/about.html'));
 });
 
+app.get('/employees', function(req, res){
+res.sendFile(path.join(__dirname + '/data/employees.json'));
+});
+
+app.get('/employees', function(req, res){
+    res.sendFile(path.join(__dirname + '/data/employees.json'));
+    });
+
 /*app.listen(port, function () {
 console.log('Express http server listening on ' + port)
 console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 })*/
 
-var HTTP_PORT = process.env.port || 3000;
+var HTTP_PORT = process.env.port || 8080;
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log('listening on', app.port);
 });
 
